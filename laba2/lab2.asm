52f63f6584db109c202e7a0f7366e76f92e1de43
nxvrlxv <152263693+nxvrlxv@users.noreply.github.com>
2025-08-26 15:19:56 +0300

    Create lab2.asm

---
+++ laba2/lab2.asm
@@ -0,0 +1,104 @@
+.model small
+stack 100h
+
+.data
+    input_str   db      "Input your string: ", 0Dh, 0Ah, '$'
+    buffer      db      100
+                db      0
+                db      101 DUP(0)
+    buffer_out  db      101 DUP(0)
+
+.code
+start:
+        mov     ax, @data
+        mov     ds, ax
+        mov     es, ax
+        
+        mov     ah, 9
+        lea     dx, input_str
+        int     21h
+
+        mov     ah, 0Ah
+        mov     dx, offset buffer
+        int     21h
+        
+        mov     ah, 02h
+        mov     dl, 0Dh
+        int     21h
+        mov     dl, 0Ah
+        int     21h
+        
+        lea     si, [buffer + 2]      
+        mov     cl, [buffer + 1]      
+        lea     di, [buffer_out]      
+        xor     bx, bx
+        mov     bx, 1
+        cld
+        
+next_char:
+        lodsb                         
+        cmp     al, 0Dh               
+        je      end_prog
+
+        cmp     al, ' '
+        je      new_word
+
+        
+        cmp     bl, 12
+        jz      rever
+        test    bl, 1
+        jz      copy_char
+        jmp     next_char
+
+copy_char:
+        stosb                         
+        jmp     next_char
+
+new_word:
+        test    bl, 1
+        jz      add_space
+        jmp     skip_space
+add_space:
+        mov     al, ' '
+        stosb
+skip_space:
+        inc     bl
+        jmp next_char
+        
+rever:
+        dec     si
+             
+rever_0:     
+        lodsb
+        cmp     al, ' '
+        jnz     rever_0
+        push    si
+        sub     si,2
+        
+rever_1:
+        std
+        lodsb
+        cmp     al, ' '
+        je      rever_2
+        cld
+        stosb
+        jmp     rever_1
+        
+rever_2:
+        cld
+        mov byte ptr es:[di], 20h
+        inc     di
+        pop     si
+        inc     bl
+        jmp     next_char
+end_prog:
+        mov     al, '$'
+        stosb                         
+        
+        mov     ah, 9
+        mov     dx, offset buffer_out
+        int     21h
+        
+        mov     ah, 4Ch
+        int     21h
+end start
